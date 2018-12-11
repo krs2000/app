7d2041d7842fb347dc87a093365cad6a78b756db
krs2000 <krskrysiak@gmail.com>
2018-12-11 20:35:32 +0100

    .

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './index.css';
 import axios from 'axios';
 import Moment from 'react-moment';
+import sb from '../../assets/shark-blog.png';
 class Blog extends Component {
   constructor(props) {
     super(props);
@@ -45,6 +46,7 @@ class Blog extends Component {
   render() {
     return (
       <div className="blog" id='blog'>
+    <img className={sb} src={sb} alt="Nature" />
         <h2>Latest post</h2>
         <div className="blog-feed" onClick={() => console.log(this.state)}>
           {this.state.articles.length > 0 ? this.returnArticles() : ''}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,10 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects" id='projects'>
-      <div className="project-box"></div>
-      <div className="project-box-1"></div>
-      <div className="project-box-2">Invest Jar</div>
+      <div className="project-row"><div className="project-box"></div><h3>Filming agency website</h3></div>
+      <div className="project-row">  <div className="project-box-1"></div><h3>Crafted beers playground app</h3></div>
+      <div className="project-row"><div className="project-box-2">Invest Jar</div><h3>React.js playground app</h3></div>
+      <div className="project-row"><div className="project-box-3"></div><h3>Hi-tech company webpage in colaboration with clickdesign.pl</h3></div>
       </div>
     );
   }
