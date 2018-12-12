b1e191bfe9eac4a2ece3690ba92d4b91019c0e38
krs2000 <krskrysiak@gmail.com>
2018-12-12 21:51:04 +0100

    .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,56 +1,27 @@
 import React, { Component } from 'react';
 import './About.css';
-import html from "../../assets/html.jpg" ;
-import css from "../../assets/css.jpg" ;
-import bsp from "../../assets/bsp.jpg" ;
-import wp from "../../assets/wp.jpg" ;
-import js from "../../assets/js.jpg" ;
-import react from "../../assets/react.jpg" ;
-import redux from "../../assets/redux.jpg" ;
-import angular from "../../assets/angular.jpg" ;
-import sass from "../../assets/angular.jpg" ;
+import html from "../../assets/html.jpg";
+import css from "../../assets/css.jpg";
+import bsp from "../../assets/bsp.jpg";
+import wp from "../../assets/wp.jpg";
+import js from "../../assets/js.jpg";
+import react from "../../assets/react.jpg";
+import redux from "../../assets/redux.jpg";
+import angular from "../../assets/angular.jpg";
+import sass from "../../assets/angular.jpg";
 
 class About extends Component {
   render() {
     return (
 
       <div className="about">
-        <section id="skills" className="secondSection">
-    ABOUT 
-          {/* <button className="btn1 triangle">Front-end</button>
-          <button className="btn2">Other skills</button>
-          <button className="btn3">Wish to learn</button> */}
-        </section>
-        <section className="thirdSection">
 
-          {/* <span className="skillsBox">
-            <img src={html} alt="html" />
-            <img src={css} alt="css" />
-            <img src={sass}  alt="sass" />
-            <img src={bsp}  alt="bsp" />
-            <img src={wp}  alt="wp" />
-            <img src={js}  alt="js" />
-            <img src={react}  alt="react" />
-            <img src={redux}  alt="redux" />
-            <img src={angular} alt="angular" />
-          </span>
-          <span className="skillsText"></span> */}
-
-        </section>
-
+   <h2>ABOUT</h2>     
+    <h4 className="project-row">If you need a website or looking for front-end developer for your projects</h4>
       
-               {/* <p>    <span class="nice-font">Hello World ,</span> I am a front-end programmer with technology background, based in Poland. 
-                My favorable technology is javascript, but I am always open to learn new things
-                 and develop skills. I have been working with C# and SQL as well on a backend too.
-                 </p> 
-                 <p> I enjoy the pace of front-end enviroment change, and would like to contribite in internet and software creation.</p> 
-                   <p>
-                 I have been working permamently with softwarehouses on site, but also have remote experience.
-                 <p>I love creating web apps, e-commerce sites and regular webpages.
- </p>
-                 </p>  <p>
-                 If you are willing to recruite a front-end developer based
-                  in Poland or look up to remote partnership please feel free to contact.   </p>  */}
+
+
+
       </div>
 
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import './Projects.css';
 
 class Projects extends Component {
+
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      firstCard : true
+
+    };
+
+  }
+
+  change = () => {
+console.log(this.state.firstCard)
+    this.setState({ firstCard : !this.state.firstCard})
+  }
+
   render() {
     return (
       <div className="projects" id='projects'>
-      <div className="project-row"><div className="project-box"></div><h3>Filming agency website</h3></div>
-      <div className="project-row">  <div className="project-box-1"></div><h3>Crafted beers playground app</h3></div>
-      <div className="project-row"><div className="project-box-2">Invest Jar</div><h3>React.js playground app</h3></div>
-      <div className="project-row"><div className="project-box-3"></div><h3>Hi-tech company webpage in colaboration with clickdesign.pl</h3></div>
+      <h2>Projects</h2>
+        <div className='triangle-box'>
+          <button  className={this.state.firstCard ? "btn1 triangle" : "btn1 triangle triangle-1"} onClick={()=> this.change()}>Clients</button>
+          <button className="btn-2" onClick={()=> this.change()}>Personal</button>
+        </div>
+      { !this.state.firstCard &&  <div className="project-row-box">
+        <div className="project-row">  <div className="project-box-1"></div><h3>Crafted beers playground app</h3></div>
+        <div className="project-row"><div className="project-box-2">Invest Jar</div><h3>Investing playground app</h3></div>
+    </div> }
+       { this.state.firstCard  && <div className="project-row-box">
+        <div className="project-row"><div className="project-box"></div><h3>Filming agency website</h3></div>
+        <div className="project-row"><div className="project-box-3"></div><h3>Hi-tech company webpage in colaboration with clickdesign.pl</h3></div>
+    </div> }
       </div>
     );
   }
