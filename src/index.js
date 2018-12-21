06980d44148bfd8bcfc3dea047b16f885e52ad53
krs2000 <krskrysiak@gmail.com>
2018-12-21 08:56:56 +0100

    .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/wickedcss.min.css';
 // import './styles/animate.css';
-import "react-alice-carousel/lib/alice-carousel.css";
+// import "react-alice-carousel/lib/alice-carousel.css";
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
